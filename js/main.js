8791d2277ea79518ab50b9fc922cfd357e681b24
yeshihao <706722153@qq.com>
2018-06-13 13:38:13 +0800

    1

--- js/main.js
+++ js/main.js
@@ -55,3 +55,22 @@
 (function() {
   var demoItems = document.querySelectorAll('.grid-item')
 }());
+
+
+const myTheme = {
+  render(state, instance) {
+    const container = document.createElement('div')
+    container.lang = "en-US"
+    container.className = 'gitment-container gitment-root-container'
+    container.appendChild(instance.renderHeader(state, instance))
+    container.appendChild(instance.renderEditor(state, instance))
+    container.appendChild(instance.renderComments(state, instance))
+    container.appendChild(instance.renderFooter(state, instance))
+    return container
+  },
+}
+const gitment = new Gitment({
+  // ...
+  theme: myTheme,
+})
+gitment.render('container')
