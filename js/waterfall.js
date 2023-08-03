dbff674048f754cb24493042efbbbdd69ed4bc26
woshiyeshihao123 <706722153@qq.com>
2023-08-03 08:05:22 +0800

    修改了域名

--- js/waterfall.js
+++ js/waterfall.js
@@ -14,42 +14,42 @@
      */
   var demoContent = [
     {
-      demo_link: 'http://www.14how.com/demo-lib/quorrajs-index/',
+      demo_link: '/demo-lib/quorrajs-index/',
       img_link: 'https://i.loli.net/2018/06/12/5b1f6df8e8648.png',
       code_link: 'https://github.com/woshiyeshihao123/demo-lib/blob/master/small-tasks/quorrajs-index/',
       title: '仿quorrajs动态头部NAV',
       core_tech: 'jQuery BootStrap CSS3',
       description: '还原了一个简单的 CSS3 动画效果，当页面滚动到指定距离时，header 区的背景色由透明变为蓝色,并且缩小高度。仿照了网站 <a href ="https://quorrajs.org/">https://quorrajs.org/</a> 的 Header 区动画效果。'
     }, {
-      demo_link: 'http://www.14how.com/demo-lib/Chorme-input/',
+      demo_link: '/demo-lib/Chorme-input/',
       img_link: 'https://i.loli.net/2018/06/19/5b28904160ba3.png',
       code_link: 'https://github.com/woshiyeshihao123/demo-lib/tree/master/small-tasks/Chorme-input',
       title: '在chrome下禁止input中文输入',
       core_tech: 'jQuery CSS',
       description: '临时办法,只在Chrome下斟酌使用,利用 password 只支持数字，字母，特殊字符的特性，使 Chrome 兼容实现 禁用输入法 禁止中文输入 的功能。'
     }, {
-      demo_link: 'http://www.14how.com/demo-lib/adrock/',
+      demo_link: '/demo-lib/adrock/',
       img_link: 'https://i.loli.net/2018/09/18/5ba0c457ccec7.png',
       code_link: 'https://github.com/woshiyeshihao123/demo-lib/tree/master/small-tasks',
       title: 'lottie',
       core_tech: 'AE lottie html js css',
       description: 'lottie实现AE动画自动生成SVG动画代码'
     }, {
-      demo_link: 'http://www.14how.com/demo-lib/localstore/',
+      demo_link: '/demo-lib/localstore/',
       img_link: 'https://i.loli.net/2018/09/18/5ba0c2ad084bb.png',
       code_link: 'https://github.com/woshiyeshihao123/woshiyeshihao123.github.io/tree/master/demo-lib/localstore',
       title: '本地增删改,仿数据库应用',
       core_tech: 'JavaScript LocalStorage fis3 Sass',
       description: '本地缓存实现的仿数据库下的增删改'
     },{
-      demo_link: 'http://www.14how.com/demo-lib/ghost-button-css3',
+      demo_link: '/demo-lib/ghost-button-css3',
       img_link: 'http://7q5cdt.com1.z0.glb.clouddn.com/DemoGhost-Button.png',
       code_link: 'https://github.com/woshiyeshihao123/demo-lib/tree/master/small-tasks/ghost-button-css3',
       title: 'Ghost Button 幽灵按钮',
       core_tech: 'CSS3',
       description: '使用 CSS3 中的旋转、缩放、过渡、动画等效果，制作出很酷的按钮效果。'
     }, {
-      demo_link: 'http://www.14how.com/demo-lib/shadow-demo-css3',
+      demo_link: '/demo-lib/shadow-demo-css3',
       img_link: 'http://7q5cdt.com1.z0.glb.clouddn.com/Demoshadow.png',
       code_link: 'https://github.com/woshiyeshihao123/demo-lib/tree/master/small-tasks/shadow-demo-css3',
       title: 'CSS3 阴影特效',
